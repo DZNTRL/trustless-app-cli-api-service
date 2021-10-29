e897af3f7108d9320b6d480c03160058971608b2
joshuaebowling <joshuaebowling@gmail.com>
2021-10-29 11:00:39 -0400

    JBOT-41 adjust after refactoring core/common

--- src/User.ts
+++ src/User.ts
@@ -1,5 +1,5 @@
 import { IResponse } from "pro-web-common/dist/js/interfaces/IResponse"
-import Core from "pro-web-core"
+import { Response } from "pro-web-common/dist/js/Response"
 import { Base } from "./base"
 
 
@@ -12,7 +12,7 @@ export class User extends Base {
     }
     async checkUsernameUnique(username: string) {
         const result = await this.get<IResponse<boolean>>(`${this.baseUrl}/api/user/unique/${username}`)
-        const resp = new Core.Response<boolean>()
+        const resp = new Response<boolean>()
         resp.Data = result.data.Data
         resp.Message = result.data.Message
         resp.IsError = result.data.IsError
@@ -20,12 +20,12 @@ export class User extends Base {
 
     }
     async requestLogin(username: string) {
-        return this.get(`${this.baseUrl}/request-session/${username}`) as unknown as Promise<IResponse<string>>
+        return this.get(`${this.baseUrl}/api/request-session/${username}`) as unknown as Promise<IResponse<string>>
     }
     async login(username: string, challenge: string) {
-        return this.post(`${this.baseUrl}/login/${username}`, {username, password: challenge}) as unknown as Promise<IResponse<boolean>>
+        return this.post(`${this.baseUrl}/api/login/${username}`, {username, password: challenge}) as unknown as Promise<IResponse<boolean>>
     }
     async createUser(username: string, publicKey: string) {
-        return this.post(`${this.baseUrl}/user/`, {username, publicKey}) as unknown as Promise<IResponse<number>>
+        return this.post(`${this.baseUrl}/api/user/`, {username, publicKey}) as unknown as Promise<IResponse<number>>
     }
 }
